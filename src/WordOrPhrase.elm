12c7a6e552ba7bd6973e35083ab55272838fec24
Nicholas Montano <montanonic@gmail.com>
2021-11-03 11:37:37 -0700

    flesh out basics for WordOrPhrase structure to replace Word

---
+++ src/WordOrPhrase.elm
@@ -0,0 +1,66 @@
+module WordOrPhrase exposing (..)
+
+import List.Extra as ListE
+
+
+
+{- What is the real difference between a word an a phrase in terms of how it's used in a language?
+
+   They are in many ways one-in-the-same, and I can see why LingQ would just call these lingqs.
+
+   I might (cheekily) call them WOPs, wordOrPhrases. wordsAndPhrases is more correct since it is a
+   union of words or phrases, yielding both things :). Naming shiz aside, currently the main obvious
+   difference is that a phrase must reference a list of words. And we might wish to only lookup
+   phrases, separate from words. So let's design a custom data structure that has a convenient
+   interface for yielding both.
+
+   Do we *need* to know if something is a phrase? Or is that something we can just ask for when we
+   need. I'm guessing the latter. For example, we have selectedWord, which can change to selectedWOP
+   without any real loss. And then in the places where we *want* to use the phrase information, we
+   just can.
+
+   One thing we need to handle still is de-tashkylization. Perhaps one way to do this is have an
+   option fullTashkyl field, where I can manually verify what the full proper version is, and then
+   *any* words will match *unless* they have tashkyl markings that contradict any of the official
+   versions. But how do we do a dictionary lookup in this case of partial tashkyl? We can of course
+   just look up the word without tashkyl. This gets really tricky. I'm not sure I'm ready to solve
+   this yet.
+-}
+
+
+{-| A word or a phrase, behind a uniform data structure.
+-}
+type alias WOP =
+    { wordOrPhrase : List String -- (must not be empty) a singleton is a word, otherwise is a phrase
+    , definitions : List String
+    , notes : String
+    , tags : List String
+    }
+
+
+setDefinition : Int -> String -> WOP -> WOP
+setDefinition defNumber definition wop =
+    { wop | definitions = ListE.setAt defNumber definition wop.definitions }
+
+
+setNotes : String -> WOP -> WOP
+setNotes notes wop =
+    { wop | notes = notes }
+
+
+{-| For a word the key is just the word, and for a phrase like "لو سمحت", the key will be that whole
+string with the spaces in between.
+-}
+key : WOP -> String
+key { wordOrPhrase } =
+    String.join " " wordOrPhrase
+
+
+isWord : WOP -> Bool
+isWord { wordOrPhrase } =
+    List.length wordOrPhrase == 1
+
+
+isPhrase : WOP -> Bool
+isPhrase { wordOrPhrase } =
+    List.length wordOrPhrase > 1
