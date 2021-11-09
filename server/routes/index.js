be9e10f8f7d4a0683d17bec2b7cd3fb5f113e560
Nicholas Montano <montanonic@gmail.com>
2021-11-08 22:11:37 -0800

    add support for server rendering and netlify deploy

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var fs = require('fs');
+var path = require('path');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// I can add the ability to server-side save, soon.
+// router.get('/initializeData', function (req, res, next) {
+//   var rawData = fs.readFileSync(path.join(__dirname, '../data/backup_19.json'));
+//   res.send(JSON.parse(rawData));
+// });
 
 router.post('/updateAudioName', function (req, res, next) {
   const { old, new: new_ } = req.body;
